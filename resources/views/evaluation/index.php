3a985ac5c645fd6f8015e6345ab2082c3e99eecc
chanachai_c <macishere2539gmail.com>
2020-04-26 15:33:55 +0700

    update index-evaluation

--- resources/views/evaluation/index.php
+++ resources/views/evaluation/index.php
@@ -64,7 +64,6 @@
 							<?php if($current_employee->id_position == 2 && $current_employee->id_department == "hr0001" ):?>
 								<th>กำหนดระยะเวลาการประเมิน</th>
 							<?php endif ?>
-							<th>กำหนดเวลา</th>
 							<th>ดู</th>
 							<?php if($current_employee->id_position == 2 && $current_employee->id_department == "hr0001" ):?>
 								<th>ลบ</th>
@@ -83,8 +82,6 @@
 								<?php if($current_employee->id_position == 2):?>
 									<td><a href="<?php echo route('evaluation.human_assessment.get', $evaluation->id_topic)?>"><button type="button" class='btn btn-warning assessment '><i class="fa fa-check-square-o"></i> ประเมิน</button></a></td>
 								<?php endif ?>
-								<td><a href="<?php echo route('evaluation.view_create_evaluations_for_index.get', $evaluation->id_topic) ?>"><i class="fa fa-eye fa-lg view-create-evaluation" style="color: black;" data-id="<?php echo $evaluation["id_topic"]?>"></i></a></td>
-
 								<td><i class="btn fa fa-clock-o fa-lg set_time"></i></td>
 								<td>
 									<!-- <input type="text" id="value_id_eval" name="value_id_eval" value="<?php /*echo $evaluation->id_topic*/ ?>" class="hidden"> -->
