1a151bd9543b3de8c88c21e420e2b9d488d0647f
chanachai_c <macishere2539gmail.com>
2020-06-13 23:11:44 +0700

    Update dataTable

--- resources/views/time_stamp/time_stamp_request.php
+++ resources/views/time_stamp/time_stamp_request.php
@@ -7,20 +7,7 @@
 <section class="content">
 	<div class="row">
 		<div class="col-md-12">
-			<div class="box box-danger">
-				<div class="box-header">
-					รายการคำร้อง
-					<!-- <div class="box-tools">
-						<div class="input-group input-group-sm" style="width: 150px;">
-							<input type="text" name="table_search" class="form-control pull-right" placeholder="ค้นหาชื่อ">
-
-							<div class="input-group-btn">
-								<button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-							</div>
-						</div>
-					</div> -->
-				</div>
-
+			<div class="box box-danger"><br>
 				<div class="box-body table-responsive no-padding">
 					<table id="myTable" class="table table-hover table-striped table-bordered">
 						<thead>
