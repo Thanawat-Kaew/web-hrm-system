0818f4c031634a2ab0a3b579bb7eb262af372320
Art <69575787+Thanawat-Kaew@users.noreply.github.com>
2021-09-10 08:04:26 +0700

    Update index.php

--- resources/views/time_stamp/index.php
+++ resources/views/time_stamp/index.php
@@ -31,36 +31,6 @@
 
 				<!-- /.box-header -->
 				<div class="box-body table-responsive no-padding">
-					<!-- <table class="table table-hover">
-						<tr>
-							<th>Name</th>
-							<th>Date</th>
-							<th>Time-In</th>
-							<th>Break-Out</th>
-							<th>Break-In</th>
-							<th>Time-Out</th>
-
-						</tr>
-						<?php //d($data->toArray());?>
-						<?php foreach($data as $value) : ?>
-							<tr>
-								<?php
-									$time_in 	= $value->requesttimestamp->where('request_type', 'time_in')->first();
-									$time_out 	= $value->requesttimestamp->where('request_type', 'time_out')->first();
-									$break_in 	= $value->requesttimestamp->where('request_type', 'break_in')->first();
-									$break_out 	= $value->requesttimestamp->where('request_type', 'break_out')->first();
-// d($time_in->request_time);sd('sdfsdfsdf');
-								?>
-								<td><?php echo $value->employee['first_name']?> <?php echo $value->employee['last_name']?></td>
-								<td><?php echo $value['date']?></td>
-								<td><?php echo empty($value['time_in']) ? (!empty($time_in)  ? "<span class='text-warning'>".$time_in->request_time  : "").'</span>' : $value['time_in']  ?></td>
-								<td><?php echo empty($value['break_out']) ? (!empty($break_out) ? "<span class='text-warning'>".$break_out->request_time  : "").'</span>' : $value['break_out']   ?></td>
-								<td><?php echo empty($value['break_in']) ? (!empty($break_in) ? "<span class='text-warning'>".$break_in->request_time  : "").'</span>' : $value['break_in']  ?></td>
-								<td><?php echo empty($value['time_out']) ? (!empty($time_out) ? "<span class='text-warning'>".$time_out->request_time  : "").'</span>' : $value['time_out']  ?></td>
-
-							</tr>
-						<?php endforeach ?>
-					</table> -->
 					<table id="example" class="table table-striped table-bordered table-hover" style="width:100%">
 				        <thead>
 				            <tr>
@@ -130,17 +100,6 @@
 										<?php echo !empty($value['time_out']) ? $value['time_out'] : ""?>
 									</td>
 								<?php } ?>
-
-									<!-- <td>
-										<?php //echo empty($value['break_out']) ? (!empty($break_out) ? "<span class='text-warning'>".$break_out->request_time  : "").'</span>' : $value['break_out']   ?>
-
-									</td>
-									<td>
-										<?php //echo empty($value['break_in']) ? (!empty($break_in) ? "<span class='text-warning'>".$break_in->request_time  : "").'</span>' : $value['break_in']  ?>
-									</td>
-									<td>
-										<?php //echo empty($value['time_out']) ? (!empty($time_out) ? "<span class='text-warning'>".$time_out->request_time  : "").'</span>' : $value['time_out']  ?>
-									</td> -->
 								</tr>
 							<?php }?>
 						<?php } ?>
