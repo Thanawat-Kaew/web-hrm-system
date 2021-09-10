b91af6ca49bf2eb8456b132f898e22c77d52ecc5
Art <69575787+Thanawat-Kaew@users.noreply.github.com>
2021-09-10 07:42:34 +0700

    Update EmployeeController.php

--- app/Http/Controllers/Employee/EmployeeController.php
+++ app/Http/Controllers/Employee/EmployeeController.php
@@ -31,26 +31,19 @@ class EmployeeController extends Controller
                 unset($_SESSION["get_session_topic"]);
             }
         }
-    	//$current_id = Employee::with('position', 'department')->where('id_employee', $this->employee)->first();
-        //return view('personal_info.personal_info', ['current_id' => $current_id]);
+    
         if(\Session::has('current_employee')){
         	$current_employee  = \Session::get('current_employee');
         	$name_position     = Position::with('employee')->where('id_position', $current_employee['id_position'])->first();
         	$name_department   = Department::with('employee')->where('id_department', $current_employee['id_department'])->first();
             $name_education   = Education::with('employee')->where('id_education', $current_employee['id_education'])->first();
-            //sd($name_education['name']);
-
             $request_edit_data = RequestChangeData::with('employee')->where('id_employee', $current_employee['id_employee'])->orderBy('id', 'desc')->get();
-            //sd($request_edit_data->toArray());
     	}
-        //sd($current_employee->toArray());
         $employee       = Employee::with('position', 'department', 'education')->where('id_employee', $current_employee['id_employee'])->first();
-        //sd($employee->toArray());
         $date_of_birth  = $employee->date_of_birth; // วันเกิดจากฐานข้อมูล
         $str            = strtotime(date('Y-m-d')) - (strtotime($date_of_birth)); //นำมาลบกับวันที่ปัจจุบัน
         $day            = floor($str/3600/24); // แปลงเป็นวัน
         $age            = number_format($day / 365); // แปลงเป็นอายุ
-        // sd($age);
         return $this->useTemplate('personal_info.personal_info', compact('name_position', 'name_department', 'request_edit_data', 'name_education', 'age', 'employee'));
     }
 
@@ -62,11 +55,7 @@ class EmployeeController extends Controller
             case 'getFormAmendmentEmployee': // แก้ไขข้อมูลครั้งแรก
                 $id             = $request->get('id');
                 $employee       = Employee::with('department')->with('position')->with('education')->where('id_employee', $id)->first();
-                //sd($employee->toArray());
-                //sd($employee['gender']);
-                //sd($employee->education['name']);
                 $department     = Department::all();
-                //$position       = Position::all();
                 $position       = Position::where('id_position', $employee['id_position'])->first();
                 $education      = Education::all();
                 $form_repo              = new FormAmendment;
@@ -76,14 +65,10 @@ class EmployeeController extends Controller
 
             case 'getHistoryChangeData': // ดูข้อมูล
                 $id             = $request->get('id');
-                //sd($id);
                 $employee       = RequestChangeData::with('employee')->where('id', $id)->first();
-                //sd($employee['id_employee']);
                 $emp_department    = Department::where('id_department', $employee['id_department'])->first();
                 $emp_position      = Position::where('id_position', $employee['id_position'])->first();
                 $emp_education     = Education::where('id_education', $employee['id_education'])->first();
-                //sd($emp_position->position['id_position']);
-                //sd($emp_position);
                 $form_repo              = new FormHistoryChangeData;
                 $form_amendment_emp     = $form_repo->getHistoryChangeData($emp_department, $emp_position ,$employee, $emp_education);
                 return response()->json(['status'=> 'success','data'=> $form_amendment_emp]);
@@ -92,10 +77,6 @@ class EmployeeController extends Controller
             case 'getEditAgain': // แก้ไขข้อมูลครั้งที่ 2
                 $id                = $request->get('id');
                 $employee          = RequestChangeData::with('employee')->where('id', $id)->first();
-                //sd($employee['id']);
-                //sd($employee['created_at']);
-                //$exp = explode(" ", $employee['created_at']);
-                //sd($exp[0]);
                 $emp_department    = Department::where('id_department', $employee['id_department'])->first();
                 $emp_position      = Position::where('id_position', $employee['id_position'])->first();
                 $emp_education     = Education::where('id_education', $employee['id_education'])->first();
@@ -168,13 +149,8 @@ class EmployeeController extends Controller
         $tel         = $request->get('tel');
         $reason      = $request->get('reason');
 
-        //sd($id_employee);
-        //sd($reason);
-        //sd($id);
-
         //save to database
         $edit = RequestChangeData::find($id);
-        //sd($edit);
         $edit->id_employee     = $id_employee;
         $edit->first_name      = $fname;
         $edit->last_name       = $lname;
@@ -196,10 +172,6 @@ class EmployeeController extends Controller
         {
 
         $employee           = RequestChangeData::with('employee')->where('id', $id)->first();
-
-        // date_default_timezone_set('Asia/Bangkok');
-        // $date = date('Y-m-d H:i:s');
-        // sd($date );
         if(!empty($employee))
         {
             $employee->delete();
